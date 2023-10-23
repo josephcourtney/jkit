1a1a56b879658136fbf0c3e79c1675ac342d2fc1
Joseph Courtney <joseph.m.courtney@gmail.com>
2023-10-23 16:29:54 -0400

    add async multi-processing

--- jkit/aio.py
+++ jkit/aio.py
@@ -1,247 +1,141 @@
-# In compliance with the Python Software Foundation License Version 2, the following is reproduced:
-#
-# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023 Python Software Foundation;
-# All Rights Reserved
-#
-# GenericAlias, _check_methods, and the definition of GeneralIterable are derived from
-# cpython/Lib/_collections_abc.py, Python version ~3.12.0rc1, (git short hash c163d7f)
-#
-# parts of the definition of FriendlyTaskGroup are derived from the definition of TaskGroup in
-# cpython/Lib/asyncio/taskgroups.py, Python version ~3.12.0rc1, (git short hash c163d7f)
-#
-# the definition of merge is derived from the roundrobin() recipe in the itertools documentation
-# cpython/Doc/library/itertools.rst, Python version ~3.12.0rc1, (git short hash c163d7f)
-
-
 import asyncio
-from abc import ABCMeta
-from asyncio import Task, TaskGroup
-from itertools import cycle, islice
-from collections.abc import Iterable
-from contextvars import Context
-from typing import TypeVar, TypeAlias, Coroutine, Any, Generator
+import concurrent.futures
+import random
 import sys
-from typing import TYPE_CHECKING
+from collections.abc import Iterable, Coroutine, Generator
+from itertools import cycle, islice
+from typing import Any, AsyncGenerator, Awaitable
 
-if TYPE_CHECKING:
-    from _typeshed import SupportsNext, SupportsAnext
+from rich import print
 
-
-
-GenericAlias = type(list[int])
-_T = TypeVar("_T")
-_T_co = TypeVar("_T_co", covariant=True)
-
-# Aliases imported by multiple submodules in typeshed
-if sys.version_info >= (3, 12):
-    _CoroutineLike: TypeAlias = Coroutine[Any, Any, _T]
-else:
-    _CoroutineLike: TypeAlias = Generator[Any, None, _T] | Coroutine[Any, Any, _T]
-
-# taken from cpython/Lib/_collections_abc.py
-def _check_methods(cls, method):
-    mro = cls.__mro__
-    for base_class in mro:
-        if method in base_class.__dict__:
-            if base_class.__dict__[method] is None:
-                return NotImplemented
-            return True
-    return NotImplemented
+# def desync(f, *args, max_workers=5, **kwargs):
+#     async def drain(aiter):
+#
+#     with asyncio.Runner() as runner:
 
 
-class GeneralIterable(metaclass=ABCMeta):
-    __slots__ = ()
-
-    @classmethod
-    def __subclasshook__(cls, subclass):
-        if cls is GeneralIterable:
-            return _check_methods(subclass, "__iter__", "__aiter__")
-        return NotImplemented
-
-    __class_getitem__: classmethod = classmethod(GenericAlias)
+async def ensure_async(it: Iterable | AsyncGenerator) -> AsyncGenerator:
+    """Make an async iterable from a synchronous one by awaiting before each item."""
+    if isinstance(it, AsyncGenerator):
+        async for item in it:
+            yield item
+    else:
+        for item in it:
+            yield item
 
 
-class FriendlyTaskGroup(TaskGroup):
-    """FriendlyTaskGroup."""
+def ensure_sync(it: AsyncGenerator):
+    async def drain_aiter(aiter):
+        ret = []
+        try:
+            async for item in aiter:
+                ret.append(item)
+        except Exception as e:
+            ret.append(e)
+        return ret
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @property
-    def loop(self):
-        return self._loop
-
-    def _check_accepting_tasks(self, raise_exception=True):
-        if not self._entered:
-            if raise_exception:
-                msg = f"TaskGroup {self!r} has not been entered"
-                raise RuntimeError(msg)
-            return False
-
-        if self._exiting and not self._tasks:
-            if raise_exception:
-                msg = f"TaskGroup {self!r} is finished"
-                raise RuntimeError(msg)
-            return False
-        if self._aborting:
-            if raise_exception:
-                msg = f"TaskGroup {self!r} is shutting down"
-                raise RuntimeError(msg)
-            return False
-
-        return True
-
-    def add_task(self, task: Task) -> Task:
-        """Add a task in this task group.
-
-        Parameters
-        ----------
-        task : asyncio.Task
-            The task to add to this group.
-
-        Returns
-        ----------
-        task : asyncio.Task
-            The same task provided as input.
-        """
-        self._check_accepting_tasks()  # strangely, mypy does not complain about this even though "_check_accepting_tasks" is not defined in the typeshed stub
-        # optimization: Immediately call the done callback if the task is
-        # already done (e.g. if the coro was able to complete eagerly),
-        # and skip scheduling a done callback
-        if task.done():
-            # mypy error: "FriendlyTaskGroup" has no attribute "_on_task_done"  [attr-defined]
-            self._on_task_done(task)
-        else:
-            # mypy error: "FriendlyTaskGroup" has no attribute "_tasks"  [attr-defined]
-            self._tasks.add(task)
-            # mypy error: "FriendlyTaskGroup" has no attribute "_on_task_done"  [attr-defined]
-            task.add_done_callback(self._on_task_done)
-        return task
-
-    def create_task(
-        self,
-        coro: _CoroutineLike[_T],
-        *,
-        name: str | None = None,
-        context: Context | None = None,
-    ) -> Task[_T]:
-        """Create a task in this task group. The signature matches that of `asyncio.create_task().`
-
-        Parameters
-        ----------
-        coro : Coroutine
-            The coroutine to wrap in a `Task` and schedule for execution.
-        name : str, optional
-            The name given to the task using `Task.set_name()`.
-        context : contextvars.Context, optional
-            The Context for coro to run in. If no context is provided, a copy of the current context is used.
-        """
-
-        task = (
-            self._loop.create_task(coro) if context is None else self._loop.create_task(coro, context=context)
-        )  # mypy error: "FriendlyTaskGroup" has no attribute "_loop"  [attr-defined]
-        task.set_name(name)
-        return self.add_task(task)
+    if isinstance(it, AsyncGenerator):
+        with asyncio.Runner() as runner:
+            yield from runner.run(drain_aiter(it))
+    else:
+        yield from it
 
 
+async def wobbly(it: Iterable):
+    async def _f(e):
+        await asyncio.sleep(random.random() * 0.1)
+        return e
 
-def merge(*iterables):
-    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+    awaitables = [_f(e) for e in it]
+    g = asyncio.as_completed(awaitables)
+    async with asyncio.TaskGroup():
+        return [await task for task in g]
+
+
+with asyncio.Runner() as runner:
+    print(runner.run(wobbly([1, 2, 3])))
+
+
+sys.exit()
+
+
+def merge(*iterables: Iterable[Iterable]) -> Generator:
+    """Merge syncronous iterables into a one by round robin."""
     # Recipe credited to George Sakkis
     num_active = len(iterables)
     nexts = cycle(iter(it).__next__ for it in iterables)
     while num_active:
         try:
-            for next in nexts:
-                yield next()
-        except StopIteration:
+            for nxt in nexts:
+                yield nxt()
+        except StopIteration:  # noqa: PERF203
             # Remove the iterator we just exhausted from the cycle.
             num_active -= 1
             nexts = cycle(islice(nexts, num_active))
 
-async def make_iterable_async(it: SupportsNext) -> SupportsAnext:
-    """make an async iterable from a synchronous one by awaiting before each item
 
-    Parameters
-    ----------
-    it : Iterable
-        synchronous iterable
+def amerge(*aiters):
+    queue = asyncio.Queue(1)
+    run_count = len(aiters)
+    cancelling = False
 
-    Yields
-    ----------
-    _ : Any
-        elements yielded from the input iterable
-
-    """
-    for item in it:
-        await asyncio.sleep(0)
-        yield item
-
-
-
-async def amerge(
-    *iterables: Iterable[SupportsAnext | SupportsNext],
-):
-    """merge async iterables into a one
-
-    Parameters
-    ----------
-    async_iterables :
-        async iterable objects
-
-    Yields
-    ----------
-    _ : Any
-        elements yielded from each of the input iterables in the order they are chosen by the event loop.
-
-    >>> import asyncio
-    >>> import aio
-    >>> async def main():
-    ...     aiters = [
-    ...         aio.make_iterable_async(range(0,5)),
-    ...         aio.make_iterable_async(range(5,10)),
-    ...         aio.make_iterable_async(range(10,15)),
-    ...     ]
-    ...     async for x in aio.merge(*aiters):
-    ...         print(x)
-    ...
-    >>> asyncio.run(main())
-    0
-    10
-    5
-    1
-    11
-    6
-    2
-    12
-    7
-    3
-    13
-    8
-    4
-    14
-    9
-
-    """
-    async_iterables = [
-        it
-        if hasattr(it, '__anext__')
-        else make_iterable_async(it)
-        for it in iterables
-    ]
-    tasks = {asyncio.create_task(anext(ait)): ait for ait in async_iterables}
-    while tasks:
-        finished_tasks, _ = await asyncio.wait(
-            tasks.keys(),
-            return_when=asyncio.FIRST_COMPLETED,
-        )
-        for task in finished_tasks:
-            ait = tasks.pop(task)
-            try:
-                result = task.result()
-            except StopAsyncIteration:
-                pass
+    async def drain(aiter):
+        nonlocal run_count
+        try:
+            async for item in aiter:
+                await queue.put((False, item))
+        except Exception as e:
+            if not cancelling:
+                await queue.put((True, e))
             else:
-                yield result
-                tasks[asyncio.create_task(anext(ait))] = ait
+                raise
+        finally:
+            run_count -= 1
+
+    async def merged():
+        try:
+            while run_count:
+                raised, next_item = await queue.get()
+                if raised:
+                    cancel_tasks()
+                    raise next_item
+                yield next_item
+        finally:
+            cancel_tasks()
+
+    def cancel_tasks():
+        nonlocal cancelling
+        cancelling = True
+        for t in tasks:
+            t.cancel()
+
+    tasks = []
+    for aiter in aiters:
+        _aiter = ensure_async(aiter)
+        tasks.append(
+            asyncio.create_task(drain(_aiter)),
+        )
+    return merged()
+
+
+async def raising_aiter():
+    for i, e in enumerate("abcdefg"):
+        if i == 3:
+            msg = "whoopsie"
+            raise ValueError(msg)
+        yield e
+
+
+async def main() -> None:
+    print()
+    async for e in amerge(
+        raising_aiter(),
+        "D",
+        "EF",
+    ):
+        print(e)
+
+
+if __name__ == "__main__":
+    with asyncio.Runner() as runner:
+        runner.run(main())
