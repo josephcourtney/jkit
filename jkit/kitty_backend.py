ce211dbf0ca5cbd9c94ef269a25529bd9f58880e
Joseph Courtney <joseph.m.courtney@gmail.com>
2024-07-18 08:54:06 -0400

    add matplotlib backend for kitty image protocol

--- jkit/kitty_backend.py
+++ jkit/kitty_backend.py
@@ -1,61 +1,108 @@
-import os
+import base64
+import fcntl
+import io
+import math
 import sys
+import termios
+import time
+import zlib
+from array import array
+from collections.abc import Callable, Iterator
+from contextlib import contextmanager
+from dataclasses import dataclass
+from shutil import get_terminal_size
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from PIL import Image
+import shutil
+import subprocess  # noqa: S404 # each use is secure as long as PATH environment variable hasn't been manipulated
+import sys
 from io import BytesIO
-import subprocess
-
-from matplotlib import interactive, is_interactive
-from matplotlib._pylab_helpers import Gcf
-from matplotlib.backend_bases import (_Backend, FigureManagerBase)
-from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 import matplotlib.pyplot as plt
-plt.style.use('dark_background')
 
-# XXX heuristic for interactive repl
+# private name import is OK since this is implementing a backend which requires access to matplotlib internals
+from matplotlib import _api, interactive, is_interactive  # noqa: PLC2701
+from matplotlib._pylab_helpers import Gcf  # noqa: PLC2701
+from matplotlib.backend_bases import FigureManagerBase, _Backend  # noqa: PLC2701
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+plt.style.use("dark_background")
+
+# heuristic for interactive repl
 if sys.flags.interactive:
-    interactive(True)
+    interactive(True)  # noqa: FBT003 # defined in matplotlib api
 
 
 class FigureManagerICat(FigureManagerBase):
     def show(self):
-        tput_lines = subprocess.run(['tput', 'lines'], capture_output=True, text=True)
+        tput_path = shutil.which("tput")
+        tput_lines = subprocess.run(
+            [tput_path, "lines"],  # noqa: S603
+            capture_output=True,
+            text=True,
+            check=False,
+        )
         rows = int(tput_lines.stdout.rstrip())
 
-        icat_px = subprocess.run(['kitty', '+kitten', 'icat', '--print-window-size'], capture_output=True, text=True)
-        px = [int(e) for e in icat_px.stdout.rstrip().split('x')]
-        px[1] -= (3 * px[1])//rows
+        kitty_path = shutil.which("kitty")
+        icat_px = subprocess.run(
+            [  # noqa: S603
+                kitty_path,
+                "+kitten",
+                "icat",
+                "--print-window-size",
+            ],
+            capture_output=True,
+            text=True,
+            check=False,
+        )
+        px = [int(e) for e in icat_px.stdout.rstrip().split("x")]
+        px[1] -= (3 * px[1]) // rows
 
         dpi = self.canvas.figure.dpi
-        w_raw, h_raw = px[0]/dpi, px[1]/dpi
+        w_raw, h_raw = px[0] / dpi, px[1] / dpi
 
-        aspect_ratio = self.canvas.figure.get_figheight() / self.canvas.figure.get_figwidth()
-        if aspect_ratio > 1:
-            self.canvas.figure.set_size_inches(h_raw / aspect_ratio, h_raw)
+        w_mult = self.canvas.figure.get_figwidth()
+        h_mult = self.canvas.figure.get_figheight()
+        aspect_ratio = h_mult / w_mult
+
+        if plt.rcParams["figure.figsize"] == [w_mult, h_mult]:
+            if aspect_ratio > 1:
+                self.canvas.figure.set_size_inches(h_raw / aspect_ratio, h_raw)
+            else:
+                self.canvas.figure.set_size_inches(w_raw, w_raw * aspect_ratio)
         else:
-            self.canvas.figure.set_size_inches(w_raw, w_raw * aspect_ratio)
+            self.canvas.figure.set_size_inches(w_raw * w_mult, h_raw * h_mult)
 
         with BytesIO() as buf:
-            self.canvas.figure.savefig(buf, format='png', transparent=True)
-            subprocess.run(['kitty', '+kitten', 'icat', '--align', 'left'], input=buf.getbuffer())
+            self.canvas.figure.savefig(buf, format="png", transparent=True)
+            subprocess.run(
+                [  # noqa: S603
+                    kitty_path,
+                    "+kitten",
+                    "icat",
+                    "--align",
+                    "center",
+                ],
+                input=buf.getbuffer(),
+                stdout=sys.stderr,  # output ot stderr to avoid being piped by default
+                check=False,
+            )
 
 
 class FigureCanvasICat(FigureCanvasAgg):
-    manager_class = FigureManagerICat
+    manager_class = _api.classproperty(lambda _: FigureManagerICat)
 
 
 @_Backend.export
 class _BackendICatAgg(_Backend):
-
     FigureCanvas = FigureCanvasICat
     FigureManager = FigureManagerICat
 
-    # Noop function instead of None signals that
-    # this is an "interactive" backend
-    mainloop = lambda: None
-
-    # XXX: `draw_if_interactive` isn't really intended for
-    # on-shot rendering. We run the risk of being called
+    # `draw_if_interactive` isn't really intended for
+    # one-shot rendering. We run the risk of being called
     # on a figure that isn't completely rendered yet, so
     # we skip draw calls for figures that we detect as
     # not being fully initialized yet. Our heuristic for
--- jkit/util.py
+++ jkit/util.py
@@ -1,25 +1,25 @@
-from datetime import datetime, timedelta
 import itertools
+from collections.abc import Callable, Iterable
+from datetime import datetime
+from typing import Any
+
 import tzlocal
+
+NotDefined = object()
+
 
 def nop(*args, **kwargs) -> None:  # noqa: ARG001
     pass
 
+
 def chunked_iterator(ary, chunk_shape, overlap):
-    slices = [list() for _ in range(len(chunk_shape))]
+    slices = [[] for _ in range(len(chunk_shape))]
     for i in range(len(chunk_shape)):
-        for j in range(0, ary.shape[i], chunk_shape[i]-overlap[i]):
-            slices[i].append(slice(j, min(ary.shape[i]-1, j+chunk_shape[i])))
+        for j in range(0, ary.shape[i], chunk_shape[i] - overlap[i]):
+            slices[i].append(slice(j, min(ary.shape[i] - 1, j + chunk_shape[i])))
     for chnk in itertools.product(*slices):
         yield tuple([slc.start for slc in chnk]), ary[chnk]
 
-
-def random_pop(l):
-    assert type(l) is list
-    i = np.random.randint(len(l))
-    e = l[i]
-    l.pop(i)
-    return e
 
 def format_timedelta(td):
     days = td.days
@@ -36,7 +36,60 @@ def format_timedelta(td):
     parts.append(f"{seconds:06.3f}")
     return ":".join(parts)
 
-def now():
+
+def now_here():
     return datetime.now(tz=tzlocal.get_localzone())
+
+
+def suppressing_map(
+    function: Callable,
+    iterable: Iterable,
+    exceptions: BaseException | tuple[BaseException, ...] | None = None,
+    default: Any = NotDefined,  # noqa: ANN401 # This is intentionally allowed to by anything
+) -> Iterable[Any]:
+    """
+    Apply a function to each element in the iterable, suppressing specified exceptions.
+
+    Parameters
+    ----------
+    function : Callable
+        The function to apply to each element in the iterable.
+    iterable : Iterable
+        The iterable containing the elements to process.
+    exceptions : Exception | tuple[Exception], optional
+        The exception types to suppress. If None (default), all exceptions are suppressed.
+    default : Any, optional
+        The default value to yield when an exception occurs. If not supplied (default), the item is skipped.
+
+    Yields
+    ------
+    any
+        The result of applying the function to each element in the iterable, or the default value if an
+            exception occurs.
+
+    Notes
+    -----
+    If exceptions is not provided or is None, all exceptions are suppressed.
+    If default is provided and an exception occurs, the default value is yielded instead of raising the
+        exception.
+    If default is None, the item is skipped when an exception occurs.
+
+    Examples
+    --------
+    >>> from math import sqrt
+    >>> iterable = [1, 4, 9, -1, 16]
+    >>> list(suppressing_map(sqrt, iterable, exceptions=ValueError, default=None))
+    [1.0, 2.0, 3.0, None, 4.0]
+    """
+    if exceptions is None:
+        exceptions = (BaseException,)
+    for item in iterable:
+        try:
+            yield function(item)
+        except exceptions:
+            if default is not NotDefined:
+                yield default
+            else:
+                continue
 
 
